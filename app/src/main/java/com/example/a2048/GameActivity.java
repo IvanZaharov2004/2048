e2a555b2c5cb2fcfec16e165ab7734ca514bf049
IvanZaharov2004 <ivan.aleks.zakharov@gmail.com>
2023-05-23 00:15:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a2048/GameActivity.java
@@ -0,0 +1,258 @@
+package com.example.a2048;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class GameActivity extends AppCompatActivity {
+    protected SharedPreferences sharedPref;
+    protected ImageView iv1_1;
+    protected ImageView iv1_2;
+    protected ImageView iv1_3;
+    protected ImageView iv1_4;
+    protected ImageView iv2_1;
+    protected ImageView iv2_2;
+    protected ImageView iv2_3;
+    protected ImageView iv2_4;
+    protected ImageView iv3_1;
+    protected ImageView iv3_2;
+    protected ImageView iv3_3;
+    protected ImageView iv3_4;
+    protected ImageView iv4_1;
+    protected ImageView iv4_2;
+    protected ImageView iv4_3;
+    protected ImageView iv4_4;
+    protected ImageView img_no_btn;
+    protected int score;
+    protected TextView tv_score;
+    protected Board TileMap;
+    public Bitmap temp;
+    public Bitmap n0;
+    public Bitmap n2;
+    public Bitmap n4;
+    public Bitmap n8;
+    public Bitmap n16;
+    public Bitmap n32;
+    public Bitmap n64;
+    public Bitmap n128;
+    public Bitmap n256;
+    public Bitmap n512;
+    public Bitmap n1024;
+    public Bitmap n2048;
+    public String score_text;
+
+    public final float sensitivity = 100;
+
+    protected GestureDetector.SimpleOnGestureListener simpleOnGestureListener = new GestureDetector.SimpleOnGestureListener() {
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+                               float velocityY) {
+            if ((e1.getX() - e2.getX()) > sensitivity) {
+                left();
+            } else if ((e2.getX() - e1.getX()) > sensitivity) {
+                right();
+            } else if ((e1.getY() - e2.getY()) > sensitivity) {
+                up();
+            } else if ((e2.getY() - e1.getY()) > sensitivity) {
+                down();
+            }
+            return true;
+        }
+    };
+
+    protected GestureDetector gestureDetector = new GestureDetector(getBaseContext(), simpleOnGestureListener);
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return gestureDetector.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_game);
+        sharedPref = getSharedPreferences(Keys.PREFERENCE_FILE_KEY, Context.MODE_PRIVATE);
+        boolean is_continue = getIntent().getBooleanExtra(Keys.SAVE_KEY, false);
+        if (is_continue)
+            TileMap = new Board(sharedPref.getString(Keys.SAVE_KEY, Keys.DEFLATE_BOARD_STATE));
+        else
+            TileMap = new Board();
+        iv1_1 = findViewById(R.id.cell_1_1);
+        iv1_2 = findViewById(R.id.cell_1_2);
+        iv1_3 = findViewById(R.id.cell_1_3);
+        iv1_4 = findViewById(R.id.cell_1_4);
+        iv2_1 = findViewById(R.id.cell_2_1);
+        iv2_2 = findViewById(R.id.cell_2_2);
+        iv2_3 = findViewById(R.id.cell_2_3);
+        iv2_4 = findViewById(R.id.cell_2_4);
+        iv3_1 = findViewById(R.id.cell_3_1);
+        iv3_2 = findViewById(R.id.cell_3_2);
+        iv3_3 = findViewById(R.id.cell_3_3);
+        iv3_4 = findViewById(R.id.cell_3_4);
+        iv4_1 = findViewById(R.id.cell_4_1);
+        iv4_2 = findViewById(R.id.cell_4_2);
+        iv4_3 = findViewById(R.id.cell_4_3);
+        iv4_4 = findViewById(R.id.cell_4_4);
+
+        img_no_btn = findViewById(R.id.img_no_btn);
+        tv_score = findViewById(R.id.score);
+
+
+        score_text = getResources().getString(R.string.score);
+        load_textures();
+        update();
+    }
+
+    public void load_textures() {
+        try {
+            temp = BitmapFactory.decodeStream(this.getAssets().open("tiles/temp.png"));
+            n0 = BitmapFactory.decodeStream(this.getAssets().open("tiles/0.png"));
+            n2 = BitmapFactory.decodeStream(this.getAssets().open("tiles/2.png"));
+            n4 = BitmapFactory.decodeStream(this.getAssets().open("tiles/4.png"));
+            n8 = BitmapFactory.decodeStream(this.getAssets().open("tiles/8.png"));
+            n16 = BitmapFactory.decodeStream(this.getAssets().open("tiles/16.png"));
+            n32 = BitmapFactory.decodeStream(this.getAssets().open("tiles/32.png"));
+            n64 = BitmapFactory.decodeStream(this.getAssets().open("tiles/64.png"));
+            n128 = BitmapFactory.decodeStream(this.getAssets().open("tiles/128.png"));
+            n256 = BitmapFactory.decodeStream(this.getAssets().open("tiles/256.png"));
+            n512 = BitmapFactory.decodeStream(this.getAssets().open("tiles/512.png"));
+            n1024 = BitmapFactory.decodeStream(this.getAssets().open("tiles/1024.png"));
+            n2048 = BitmapFactory.decodeStream(this.getAssets().open("tiles/2048.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Bitmap get_texture(int n) {
+        if (n == 0) {
+            return n0;
+        } else if (n == 2) {
+            return n2;
+        } else if (n == 4) {
+            return n4;
+        } else if (n == 8) {
+            return n8;
+        } else if (n == 16) {
+            return n16;
+        } else if (n == 32) {
+            return n32;
+        } else if (n == 64) {
+            return n64;
+        } else if (n == 128) {
+            return n128;
+        } else if (n == 256) {
+            return n256;
+        } else if (n == 512) {
+            return n512;
+        } else if (n == 1024) {
+            return n1024;
+        } else if (n == 2048) {
+            return n2048;
+        } else {
+            return temp;
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void update() {
+        iv1_1.setImageBitmap(get_texture(TileMap.map.get(0).get(0)));
+        iv1_2.setImageBitmap(get_texture(TileMap.map.get(0).get(1)));
+        iv1_3.setImageBitmap(get_texture(TileMap.map.get(0).get(2)));
+        iv1_4.setImageBitmap(get_texture(TileMap.map.get(0).get(3)));
+        iv2_1.setImageBitmap(get_texture(TileMap.map.get(1).get(0)));
+        iv2_2.setImageBitmap(get_texture(TileMap.map.get(1).get(1)));
+        iv2_3.setImageBitmap(get_texture(TileMap.map.get(1).get(2)));
+        iv2_4.setImageBitmap(get_texture(TileMap.map.get(1).get(3)));
+        iv3_1.setImageBitmap(get_texture(TileMap.map.get(2).get(0)));
+        iv3_2.setImageBitmap(get_texture(TileMap.map.get(2).get(1)));
+        iv3_3.setImageBitmap(get_texture(TileMap.map.get(2).get(2)));
+        iv3_4.setImageBitmap(get_texture(TileMap.map.get(2).get(3)));
+        iv4_1.setImageBitmap(get_texture(TileMap.map.get(3).get(0)));
+        iv4_2.setImageBitmap(get_texture(TileMap.map.get(3).get(1)));
+        iv4_3.setImageBitmap(get_texture(TileMap.map.get(3).get(2)));
+        iv4_4.setImageBitmap(get_texture(TileMap.map.get(3).get(3)));
+        score = 0;
+        for (ArrayList<Integer> i : TileMap.map) {
+            for (int j : i) {
+                score += j   ;
+            }
+        }
+        tv_score.setText(score_text + ": " + score);
+    }
+
+    public void up() {
+        TileMap.transpose();
+        TileMap.cover_up();
+        TileMap.merge();
+        TileMap.cover_up();
+        TileMap.transpose();
+        TileMap.add_tile();
+        update();
+    }
+
+    public void down() {
+        TileMap.transpose();
+        TileMap.reverse();
+        TileMap.cover_up();
+        TileMap.merge();
+        TileMap.cover_up();
+        TileMap.reverse();
+        TileMap.transpose();
+        TileMap.add_tile();
+        update();
+    }
+
+    public void left() {
+        TileMap.cover_up();
+        TileMap.merge();
+        TileMap.cover_up();
+        TileMap.add_tile();
+        update();
+    }
+
+    public void right() {
+        TileMap.reverse();
+        TileMap.cover_up();
+        TileMap.merge();
+        TileMap.cover_up();
+        TileMap.reverse();
+        TileMap.add_tile();
+        update();
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(Keys.SAVE_KEY, TileMap.toSaveString());
+        editor.apply();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        save_high_score();
+    }
+
+    public void save_high_score () {
+        int high_score = sharedPref.getInt(Keys.HIGH_SCORE_KEY , 0) ;
+        if (score > high_score) {
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putInt(Keys.HIGH_SCORE_KEY, score) ;
+            editor.apply();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/a2048/Keys.java
@@ -0,0 +1,8 @@
+package com.example.a2048;
+
+public class Keys {
+    public static final String SAVE_KEY = "SAVE_KEY";
+    public static final String PREFERENCE_FILE_KEY = "com.example.a2048.pref";
+    public static final String DEFLATE_BOARD_STATE = "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0";
+    public static final String HIGH_SCORE_KEY = "HIGH_SCORE";
+}
