1ac686923b55c6c49b7057a9f09b3d28e37c403e
IvanZaharov2004 <ivan.aleks.zakharov@gmail.com>
2023-05-30 23:28:11 +0300

    Final Update

--- app/src/test/java/com/example/a2048/ExampleUnitTest.java
+++ app/src/test/java/com/example/a2048/ExampleUnitTest.java
@@ -15,15 +15,3 @@ public class ExampleUnitTest {
         assertEquals(4, 2 + 2);
     }
 }
-
-    /* public void add_tile() {
-        int notEmpty = 0;
-        Board TileMap = new Board();
-        TileMap.add_tile();
-        for (int i = 0; i < 4; i++) {
-            if (TileMap.map.get(i).contains(2) || TileMap.map.get(i).contains(4))
-                notEmpty++;
-        }
-        assertTrue(notEmpty > 0);
-    }
-   */
